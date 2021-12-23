758056bc1f5ba94423327b88021d156da3c194a7
Halit <hozdemir@havelsan.com.tr>
2021-12-23 13:26:21 +0300

    test

--- src/main/java/tr/com/test/pageObjects/LoginPOM2.java
+++ src/main/java/tr/com/test/pageObjects/LoginPOM2.java
@@ -2,17 +2,21 @@ package tr.com.test.pageObjects;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import tr.com.test.testBase.TestBase;
 
 public class LoginPOM2 extends TestBase {
 
     public void webSitesiAc(){
 //        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/test/resources/drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--start-maximized");
-        chromeOptions.addArguments("--headless");
-        driver = new ChromeDriver(chromeOptions);
+//        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--start-maximized");
+//        chromeOptions.addArguments("--headless");
+//        driver = new ChromeDriver(chromeOptions);
+
+        System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
         driver.get("http://automationpractice.com/");
 
     }
